9eb12a55ecffd70e0aad446302ae7ac9bd0c9f00
Mishaah <96700155+Mishaah@users.noreply.github.com>
2024-06-19 16:32:13 +0200

    added control to views

--- src/views/about.vapor.vue
+++ src/views/about.vapor.vue
@@ -1,11 +1,36 @@
 <template>
-
-</template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-
-</style>
+    <body>
+        <h1>De ingeverij - about</h1>
+        <p class="subtitle">is in opbouw... <br>
+            <a title="overvloed6@hotmail.com" href="mailto:overvloed6@hotmail.com?subject=Mail from Our Site">Email Us</a>
+        </p>
+        <a @click.prevent="()=>{router.push({name:'aboutView'})}">
+            About
+        </a>
+        <br>
+        <router-link :to="{ name: 'aboutView' }">asadasd</router-link>
+        <br>
+        <router-link :to="{ name: 'homeView' }">asaaaaaaaaaaaaadasd</router-link>
+    </body>
+    
+    </template>
+    
+    <script setup>
+    
+    import { useRouter, useRoute } from 'vue-router'
+    
+    const router = useRouter()
+    const route = useRoute()
+    
+    </script>
+    
+    <style scoped>
+    p.subtitle{
+        line-height: 0.7;
+        font-size: 11;
+        font-style: italic;
+    }
+    [aria-current]{
+        background-color:hotpink;
+    }
+    </style>
--- src/views/home.vapor.vue
+++ src/views/home.vapor.vue
@@ -1,20 +1,36 @@
 <template>
-<body>
-    <h1>De ingeverij</h1>
-    <p class="subtitle">is in opbouw...</p>
-    <p>overvloed6@hotmail.com</p>
-</body>
-
-</template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-p.subtitle{
-    line-height: 0.7;
-    font-size: 11;
-    font-style: italic;
-}
-</style>
+    <body>
+        <h1>De ingeverij</h1>
+        <p class="subtitle">is in opbouw... <br>
+            <a title="overvloed6@hotmail.com" href="mailto:overvloed6@hotmail.com?subject=Mail from Our Site">Email Us</a>
+        </p>
+        <a @click.prevent="()=>{router.push({name:'aboutView'})}">
+            About
+        </a>
+        <br>
+        <router-link :to="{ name: 'aboutView' }">asadasd</router-link>
+        <br>
+        <router-link :to="{ name: 'homeView' }">asaaaaaaaaaaaaadasd</router-link>
+    </body>
+    
+    </template>
+    
+    <script setup>
+    
+    import { useRouter, useRoute } from 'vue-router'
+    
+    const router = useRouter()
+    const route = useRoute()
+    
+    </script>
+    
+    <style scoped>
+    p.subtitle{
+        line-height: 0.7;
+        font-size: 11;
+        font-style: italic;
+    }
+    [aria-current]{
+        background-color:hotpink;
+    }
+    </style>
