6c5169662180f29fc0d3d9bde533bbf579465117
Mishaah <96700155+Mishaah@users.noreply.github.com>
2024-08-12 17:09:06 +0200

    router hash change

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import home from '@/views/home.vapor.vue'
 import about from '@/views/about.vapor.vue'
 import anagrammar from '@/views/anagrammar.vapor.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-    path: '/',
-    name: 'homeView',
-    component: home
-  }, {
-    path: '/about',
-    name: 'aboutView',
-    component: about
-  }, {
-    path: '/anagrammar',
-    name: 'anagrammarView',
-    component: anagrammar
-  }]
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'homeView',
+      component: home,
+    },
+    {
+      path: '/about',
+      name: 'aboutView',
+      component: about,
+    },
+    {
+      path: '/anagrammar',
+      name: 'anagrammarView',
+      component: anagrammar,
+    },
+  ],
 })
-
 
 export default router
